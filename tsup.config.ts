f2025c5466be452b50508b882b360bbaaed6ee04
Yath Seanghay <seanghay.dev@gmail.com>
2022-06-26 14:24:24 +0700

    feat: remove generating sourcemaps

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig((options) => ({
   entry: ["src/index.ts"],
   splitting: false,
   clean: true,
-  sourcemap: true,
+  sourcemap: false,
   format: ["cjs", "esm"],
   dts: true,
   minify: !options.watch,
